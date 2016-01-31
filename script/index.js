6d172cffd2b88b25c082f0899f0600079e68a2a1
luca <lucamozzo21173@gmail.com>
2016-01-31 13:44:29 +0000

    added stylesheet added pages

--- script/index.js
+++ script/index.js
@@ -1,3 +1,8 @@
-$('.nav li').click(function(){
-    $(this).addClass('active').siblings().removeClass('active');
-})
+$(document).ready(function(){
+    $('.body-container').load('home.html');
+    $('.nav li').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        var ref = $(this).children().attr('data-href');
+        $('.body-container').load(ref);
+    })
+});
