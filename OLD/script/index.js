510fea5b8559342094e5bfdd555d9ab9b6c15fa4
lucamozzo <lucamozzo21173@gmail.com>
2016-10-31 13:35:49 +0100

    removed all old files

--- OLD/script/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.body-container').load('home.html');
-    //navbar
-    $('.nav li').click(function(){
-        $(this).addClass('active').siblings().removeClass('active');
-        var url = $(this).children().attr('data-href');
-        $('.body-container').load(url);
-    });
-});
